b93df10ca9fd702041cf14b092147686ab8cba45
qxkya <93773645+qxkya@users.noreply.github.com>
2024-03-12 16:37:37 +0100

    Update get_hwid.lua

--- get_hwid.lua
+++ get_hwid.lua
@@ -1,24 +1,28 @@
 local setclipboard = setclipboard or toclipboard or set_clipboard or (Clipboard and Clipboard.set)
 local request = request or http_request or (http and http.request) or syn.request
 
-local response = request({Url = "https://kya.jvck.gg/return-headers"}).Body
-local decoded = game:GetService("HttpService"):JSONDecode(response)
-
 local hwid = nil
 
-for i, v in pairs(decoded) do
-	local lowered = tostring(i):lower()
+if not gethwid then
+    local response = request({Url = "https://kya.jvck.gg/return-headers"}).Body
+    local decoded = game:GetService("HttpService"):JSONDecode(response)
 
-	print(i, v)
+    for i, v in pairs(decoded) do
+        local lowered = tostring(i):lower()
 
-	if (string.find(lowered, "fingerprint") or string.find(lowered, "identifier")) and not (string.find(lowered, "user")) then
-		hwid = v
-		print(i, v)
-	end
+        print(i, v)
+
+        if (string.find(lowered, "fingerprint") or string.find(lowered, "identifier")) and not (string.find(lowered, "user")) then
+            hwid = v
+            print(i, v)
+        end
+    end
+else
+    hwid = gethwid()
 end
 
 if getgenv().hwidRETURN then
-  return hwid
+    return hwid
 else
-  setclipboard(hwid)
+    setclipboard(hwid)
 end
