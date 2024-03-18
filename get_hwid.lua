bfad4c1fa97f587ddae9daa63c7978dae3ef98b2
qxkya <93773645+qxkya@users.noreply.github.com>
2024-03-18 21:03:57 +0100

    Update get_hwid.lua

--- get_hwid.lua
+++ get_hwid.lua
@@ -3,22 +3,18 @@ local request = request or http_request or (http and http.request) or syn.reques
 
 local hwid = nil
 
-if not gethwid then
-    local response = request({Url = "https://kya.jvck.gg/return-headers"}).Body
-    local decoded = game:GetService("HttpService"):JSONDecode(response)
+local response = request({Url = "https://kya.jvck.gg/return-headers"}).Body
+local decoded = game:GetService("HttpService"):JSONDecode(response)
 
-    for i, v in pairs(decoded) do
-        local lowered = tostring(i):lower()
+for i, v in pairs(decoded) do
+    local lowered = tostring(i):lower()
 
+    print(i, v)
+
+    if (string.find(lowered, "fingerprint") or string.find(lowered, "identifier")) and not (string.find(lowered, "user")) then
+        hwid = v
         print(i, v)
-
-        if (string.find(lowered, "fingerprint") or string.find(lowered, "identifier")) and not (string.find(lowered, "user")) then
-            hwid = v
-            print(i, v)
-        end
     end
-else
-    hwid = gethwid()
 end
 
 if getgenv().hwidRETURN then
