cd991a3360f428fce655a64b843881da14d8ed88
qxkya <93773645+qxkya@users.noreply.github.com>
2024-06-14 18:22:58 +0200

    Update get_hwid.lua

--- get_hwid.lua
+++ get_hwid.lua
@@ -2,6 +2,7 @@ local setclipboard = setclipboard or toclipboard or set_clipboard or (Clipboard 
 local request = request or http_request or (http and http.request) or syn.request
 
 local hwid = nil
+local name = nil
 
 local response = request({Url = "https://kya.jvck.gg/return-headers"}).Body
 local decoded = game:GetService("HttpService"):JSONDecode(response)
@@ -12,13 +13,13 @@ for i, v in pairs(decoded) do
     print(i, v)
 
     if (string.find(lowered, "fingerprint") or string.find(lowered, "identifier")) and not (string.find(lowered, "user")) then
-        hwid = v
+        hwid = v; name = i
         print(i, v)
     end
 end
 
 if getgenv().hwidRETURN then
-    return hwid
+    return name, hwid
 else
     setclipboard(hwid)
 end
